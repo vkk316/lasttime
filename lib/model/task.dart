d422df46f028d32089dd3d5faf84e2df2f6b75a3
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-08-13 16:34:45 +0700

    mock

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -5,5 +5,5 @@ class Task {
   final DateTime timeStamp;
   final int categoryID;
 
-  static const Map<int, String> categories = {0:'Garden', 1:'Cooking', 2:'Vehicle', 3:'Entertainment', 4:'Learning'};
+  static const List<String> categories = ['Garden', 'Cooking', 'Vehicle', 'Entertainment', 'Learning'];
 }
--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lasttime/model/task.dart';
+import 'package:lasttime/widget/category_tiles.dart';
+
 class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
@@ -7,13 +10,28 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int selectedIndex = 0;
   //GlobalKey>
+  //mock data
+  List<Task> tasks = [
+    Task(title: 'ล้างจาน', timeStamp: DateTime(2021, 8, 12, 14, 30), categoryID: 1),
+    Task(title: 'ตัดหญ้า', timeStamp: DateTime(2021, 8, 12, 16, 30), categoryID: 0),
+    Task(title: 'ปลูกฟ้าทะลายโจร', timeStamp: DateTime(2021, 8, 13, 8,45), categoryID: 0),
+    Task(title: 'ล้างรถ', timeStamp: DateTime(2021, 8, 13, 11,10), categoryID: 2)
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('LASTTIME'), actions: [IconButton(onPressed: () => print("HI"), icon: Icon(Icons.search))],),
-      body: Container()
-    );
+        appBar: AppBar(
+          title: Text('LASTTIME'),
+          actions: [
+            IconButton(
+                onPressed: () => print("HI"), icon: Icon(Icons.filter_alt)),
+            IconButton(
+                onPressed: () => print("HI"), icon: Icon(Icons.more_vert))
+          ],
+        ),
+        body: Column(
+          children: [CategoryTiles()],
+        ));
   }
-
 }
