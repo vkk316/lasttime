9ae9cf6a66744a033b05652b5d8f382d70e5efec
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-08-13 17:07:27 +0700

    ModalSheet

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -12,10 +12,22 @@ class _MainScreenState extends State<MainScreen> {
   //GlobalKey>
   //mock data
   List<Task> tasks = [
-    Task(title: 'ล้างจาน', timeStamp: DateTime(2021, 8, 12, 14, 30), categoryID: 1),
-    Task(title: 'ตัดหญ้า', timeStamp: DateTime(2021, 8, 12, 16, 30), categoryID: 0),
-    Task(title: 'ปลูกฟ้าทะลายโจร', timeStamp: DateTime(2021, 8, 13, 8,45), categoryID: 0),
-    Task(title: 'ล้างรถ', timeStamp: DateTime(2021, 8, 13, 11,10), categoryID: 2)
+    Task(
+        title: 'ล้างจาน',
+        timeStamp: DateTime(2021, 8, 12, 14, 30),
+        categoryID: 1),
+    Task(
+        title: 'ตัดหญ้า',
+        timeStamp: DateTime(2021, 8, 12, 16, 30),
+        categoryID: 0),
+    Task(
+        title: 'ปลูกฟ้าทะลายโจร',
+        timeStamp: DateTime(2021, 8, 13, 8, 45),
+        categoryID: 0),
+    Task(
+        title: 'ล้างรถ',
+        timeStamp: DateTime(2021, 8, 13, 11, 10),
+        categoryID: 2)
   ];
 
   @override
@@ -30,8 +42,17 @@ class _MainScreenState extends State<MainScreen> {
                 onPressed: () => print("HI"), icon: Icon(Icons.more_vert))
           ],
         ),
-        body: Column(
-          children: [CategoryTiles()],
+        body: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(title: Text(tasks[index].title), );
+          },
         ));
+  }
+  void _onPressFilterButton(){
+    showModalBottomSheet(context: context, builder: (context){
+      var cat = Task.categories;
+      return ListView.builder(itemBuilder: (BuildContext context, int index) { return ListTile(); },);
+    });
   }
 }
--- lib/widget/category_tiles.dart
+++ lib/widget/category_tiles.dart
@@ -1,30 +1,30 @@
-import 'package:flutter/material.dart';
-import 'package:lasttime/model/task.dart';
+// import 'package:flutter/material.dart';
+// import 'package:lasttime/model/task.dart';
 
-class CategoryTiles extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          ...Task.categories
-              .asMap()
-              .map((index, value) => MapEntry(
-                  index,
-                  Container(
-                    margin: EdgeInsets.all(8),
-                    child: Text(value),
-                  )))
-              .values
-              .toList()
-        ],
-      ),
-    );
-  }
+// class CategoryTiles extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       scrollDirection: Axis.horizontal,
+//       child: Row(
+//         children: [
+//           ...Task.categories
+//               .asMap()
+//               .map((index, value) => MapEntry(
+//                   index,
+//                   Container(
+//                     margin: EdgeInsets.all(8),
+//                     child: Text(value),
+//                   )))
+//               .values
+//               .toList()
+//         ],
+//       ),
+//     );
+//   }
 
-  // List<Widget> _buildCategoryBoxes(){
-  //   List<Widget> buttons;
-  //   Task.categories.asMap().map((index, value) => )
-  // }
-}
+//   // List<Widget> _buildCategoryBoxes(){
+//   //   List<Widget> buttons;
+//   //   Task.categories.asMap().map((index, value) => )
+//   // }
+// }
